c703112e9419522673c27b562dc30a3bde20fe23
danielepelleg <55688016+danielepelleg@users.noreply.github.com>
2020-04-07 14:10:35 +0200

    Update method to train algorithm

--- src/detector.py
+++ src/detector.py
@@ -5,25 +5,36 @@ import numpy as np
 
 from scraper import scrapeAccount
 
+"""
+    Train the Ada Boost algorithm with a dataset or another
+        whether an account is private or not 
+"""
+def trainAlgorithm(dictionaryLen):
+    if (dictionaryLen == 11):
+        df = pd.read_csv("./resources/DefaultDataSet.csv")
+        X = df.iloc[:, 0:11].values
 
-df = pd.read_csv("./resources/DataSet.csv")
-X = df.iloc[:, 0:11].values
-y = df[['Fake']]
+    elif (dictionaryLen == 14):
+        df = pd.read_csv("./resources/OpenDataSet.csv")
+        X = df.iloc[:, 0:14].values
+    
+    y = df[['Fake']]
 
-"""
-    Set the Train set and the Test set
-"""
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    """
+        Set the Train set and the Test set
+    """
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-"""
-    Set the Decision Tree Classifier algorithm
-"""
-#AdaBoost
-from sklearn.ensemble import AdaBoostClassifier
-clf = AdaBoostClassifier(n_estimators=100, random_state=0)
-# train the algorithm
-clf.fit(X_train, y_train.values.ravel())
+    """
+        Set the AdaBoost Classifier algorithm
+    """
+    #AdaBoost
+    from sklearn.ensemble import AdaBoostClassifier
+    clf = AdaBoostClassifier(n_estimators=100, random_state=0)
+    # train the algorithm
+    clf.fit(X_train, y_train.values.ravel())
+    return clf
 
 """
     Get the result array and transform it to a String.
@@ -39,27 +50,38 @@ def getPrediction(result):
   return switcher.get(number, "invalid")
 
 """
-    Get the probability the result is a success 
+    Get the probability of the result
     Percentage value.
 """
 def getProbability(result):
+    # Take the result 0 or 1
     prediction = result[0][0]
+    # If the User has been classified as real
     if (prediction == 0):
+        # Takes the percentage for real account
         probability = result[1][0][0]
+    # Else takes the percentage for fake account
     else: probability = result[1][0][1]
 
+    # Forma to percentage
     return'{:.1%}'.format(probability)
 
-
+"""
+    Predict the authenticity of the account
+"""
 def predict(username):
     result = []
     account = scrapeAccount(username)
-    prediction = clf.predict(account)
-    probability = clf.predict_proba(account)
-    result.append(prediction)
-    result.append(probability)
-    print(result)
+    if (account != None):
+        clf = trainAlgorithm(len(account[0]))
+        prediction = clf.predict(account)
+        probability = clf.predict_proba(account)
+        result.append(prediction)
+        result.append(probability)
+        print(result)
 
-    return result
+        return result
+        
+    else: return None
     
 
